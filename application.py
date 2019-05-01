8cf9fb88200ae54cfbdeeb9c7c6e6aaddade313e
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-05-01 23:11:22 +0100

    refactor: adding complete routing for allCategories tos showItems

--- application.py
+++ application.py
@@ -27,14 +27,17 @@ def home(): #READ
 def showCatalog(): # READ
     return render_template('allCategories.html', categories=categories)
 
+# PROTECTED
 @app.route('/catalog/new', methods=['GET', 'POST'])
 def newCategory(): # CREATE
     return "This page will create a new category"
 
+# PROTECTED
 @app.route('/catalog/<string:category_name>/edit', methods=['GET', 'POST'])
 def editCategory(category_name): # UPDATE
     return "This page will edit a category"
 
+# PROTECTED
 @app.route('/catalog/<string:category_name>/delete', methods=['GET','POST'])
 def deleteCategory(category_name): # DELETE
     return "This page will delete a category"
@@ -42,20 +45,33 @@ def deleteCategory(category_name): # DELETE
 @app.route('/catalog/<string:category_name>')
 @app.route('/catalog/<string:category_name>/items')
 def showItems(category_name): # READ
-    return "This page will show the items for a category"
+    info = []
+    for i in items:
+        data = {}
+        data['name'] = i['title']
+        data['description'] = i['description']
+        category_id = i['category_id']
+        for j in categories:
+            if j['id'] == category_id:
+                data['category'] = j['name']
+        info.append(data)
+    return render_template('showItems.html', info=info)
 
 @app.route('/catalog/<string:category_name>/items/<string:item_name>')
 def showItemDetails(category_name, item_name): # READ
     return "This page will show the item details for a category"
 
+# PROTECTED
 @app.route('/catalog/<string:category_name>/items/new', methods=['GET', 'POST'])
 def newItem(category_name): # CREATE
     return "This page will create new item for a category"
 
+# PROTECTED
 @app.route('/catalog/<string:category_name>/items/<string:item_name>/edit')
 def editItem(category_name, item_name): # UPDATE
     return "This page will edit an item for a category"
 
+# PROTECTED
 @app.route('/catalog/<string:category_name>/items/<string:item_name>/delete')
 def deleteItem(category_name, item_name): # DELETE
     return "This page will delete an item for a category"
