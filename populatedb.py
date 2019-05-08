60579dfb4787cfe8ca973b64277be8d43545e5d3
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-05-09 00:47:25 +0100

    refactor: updating db population file

--- populatedb.py
+++ populatedb.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import User, Category, Item, Base
+from database import User, Category, Item, Base
 
 engine = create_engine('sqlite:///catalog.db')
 # Bind the engine to the metadata of the Base class so that the
