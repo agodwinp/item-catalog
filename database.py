f641358a2b0227c1c48788ff0db0ab4153918cb9
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-05-09 00:47:05 +0100

    feat: adding User table for authorisations

--- database.py
+++ database.py
@@ -14,36 +14,17 @@ secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(32), index=True)
-    password_hash = Column(String(32))
-
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
-
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
-    
-    def generate_auth_token(self, expiration=600):
-        s = Serializer(secret_key, expires_in=expiration)
-        return s.dumps({'id': self.id})
-
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(secret_key)
-        try:
-            data = s.loads(token)
-        except SignatureExpired: # valid token but expired
-            return None
-        except BadSignature: # invalid token
-            return None
-        user_id = data['id']
-        return user_id
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
+    user_id = Column(String(250), ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -60,6 +41,8 @@ class Item(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
