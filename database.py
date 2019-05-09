a42d6dd09ac185cfe513587a0992d36067b41dac
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-05-09 10:24:49 +0100

    refactor: changed data type of category id to integer

--- database.py
+++ database.py
@@ -23,7 +23,7 @@ class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
-    user_id = Column(String(250), ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
