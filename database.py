f4f3307cddac7a932bbd50b0535d91ccac6983a4
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-05-21 21:45:41 +0100

    refactor: removing unneeded packages

--- database.py
+++ database.py
@@ -3,12 +3,10 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-from passlib.apps import custom_app_context as pwd_context
 import random, string
-from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
-secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
 
 
 class User(Base):
