6e8835ed2ba2fbf8905f68cda7c423f2e8da0782
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-05-22 09:14:41 +0100

    refactor: updating application.py to pass PEP 8 tests

--- application.py
+++ application.py
@@ -1,14 +1,17 @@
 # !/usr/bin/env python3
 # Import packages
 import os
-import random, string, json, httplib2
+import random
+import string
+import json
+import httplib2
 from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
 from werkzeug.utils import secure_filename
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
-from flask import make_response
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import flash, make_response
 from flask import session as login_session
 from database import Base, User, Category, Item
 from flask_httpauth import HTTPBasicAuth
@@ -20,7 +23,8 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 auth = HTTPBasicAuth()
-engine = create_engine('sqlite:///catalog.db', connect_args={'check_same_thread':False})
+engine = create_engine('sqlite:///catalog.db',
+                       connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
@@ -29,23 +33,29 @@ session = DBSession()
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 
+
 # Function used for generate state
 def generateState(sess, key):
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    state = ''.join(random.choice(string.ascii_uppercase +
+                                  string.digits) for x in range(32))
     sess[key] = state
     return state
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 # Function for serializing items in category
 def serialize_category(category):
+    items = session.query(Item).filter_by(category_id=category.id)
     return {
         'id': category.id,
         'name': category.name,
-        'items': [i.serialize for i in session.query(Item).filter_by(category_id=category.id)]
+        'items': [i.serialize for i in items]
     }
+
 
 # JSON API Endpoints
 @app.route('/catalog/json')
@@ -53,16 +63,19 @@ def catalogJSON():
     categories = session.query(Category).all()
     return jsonify(Categories=[serialize_category(i) for i in categories])
 
+
 @app.route('/catalog/<int:category_id>/json')
 @app.route('/catalog/<int:category_id>/items/json')
 def categoryJSON(category_id):
     category_items = session.query(Item).filter_by(category_id=category_id)
     return jsonify(Items=[i.serialize for i in category_items])
 
+
 @app.route('/catalog/<int:category_id>/items/<int:item_id>/json')
 def itemsJSON(category_id, item_id):
     item = session.query(Item).filter_by(id=item_id).one()
     return jsonify(Item=item.serialize)
+
 
 # Application routes
 @app.route('/', methods=['GET', 'POST'])
@@ -73,17 +86,22 @@ def landingPage():
         return render_template("loginPage.html", STATE=state)
     if request.method == 'POST':
         try:
-            # Check the state variable for extra security, state from ajax request
-            # should be the same as random string above
+            # Check the state variable for extra security
+            # state from ajax request should be the same as
+            # random string above
             if login_session['state'] != request.args.get('state'):
-                response = make_response(json.dumps('Invalid state parameter.'), 401)
+                message = "Invalid state parameter."
+                response = make_response(json.dumps(message), 401)
                 response.headers['Content-Type'] = 'application/json'
                 return response
             # If so, then retrieve token sent by client
             token = request.data
             # Request an access token from the Google API
-            idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), CLIENT_ID)
-            url = ('https://oauth2.googleapis.com/tokeninfo?id_token=%s' % token)
+            idinfo = id_token.verify_oauth2_token(token,
+                                                  google_requests.Request(),
+                                                  CLIENT_ID)
+            token_url = "https://oauth2.googleapis.com/tokeninfo?id_token=%s"
+            url = (token_url % token)
             h = httplib2.Http()
             result = json.loads(h.request(url, 'GET')[1])
             # If there was an error in the access token info, abort
@@ -94,20 +112,28 @@ def landingPage():
             # Verify that the access token is used for the intended user
             user_google_id = idinfo['sub']
             if result['sub'] != user_google_id:
-                response = make_response(json.dumps("Token's user ID does not match given user ID."), 401)
+                message = "Token's user ID does not match given user ID."
+                response = make_response(json.dumps(message), 401)
                 response.headers['Content-Type'] = 'application/json'
                 return response
             # Verify that the access token is valid for this app
             if result['aud'] != CLIENT_ID:
-                response = make_response(json.dumps("Token's client ID does not match apps."), 401)
+                message = "Token's client ID does not match apps."
+                response = make_response(json.dumps(message), 401)
                 print("Token's client ID does not match apps.")
                 response.headers['Content-Type'] = 'application/json'
                 return response
             # Check if the user is already logged in
             stored_access_token = login_session.get('access_token')
             stored_user_google_id = login_session.get('user_google_id')
-            if stored_access_token is not None and user_google_id == stored_user_google_id:
-                response = make_response(json.dumps("Current user is already connected."), 200)
+            if stored_access_token is not None:
+                if user_google_id == stored_user_google_id:
+                    message = "Current user is already connected."
+                    response = make_response(json.dumps(message), 200)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+                message = "User authentication error."
+                response = make_response(json.dumps(message), 200)
                 response.headers['Content-Type'] = 'application/json'
                 return response
             # Store the access token in the session cookie for later use
@@ -119,9 +145,10 @@ def landingPage():
             login_session['email'] = idinfo['email']
             # Check if this user exists in database
             try:
-                session.query(User).filter_by(email=login_session['email']).one()
+                session.query(User).filter_by(email=login_session['email'])
             except:
-                newUser = User(name=login_session['username'], email=login_session['email'],
+                newUser = User(name=login_session['username'],
+                               email=login_session['email'],
                                picture=login_session['picture'])
                 session.add(newUser)
                 session.commit()
@@ -130,10 +157,12 @@ def landingPage():
             # Invalid token
             return "ValueError... See trace."
 
+
 @app.route('/logout', methods=['POST'])
 def logout():
     login_session.clear()
     return "Logged out"
+
 
 @app.route('/welcome')
 def welcome():
@@ -141,12 +170,13 @@ def welcome():
     picture = login_session['picture']
     return render_template('welcome.html', name=name, picture=picture)
 
+
 @app.route('/catalog')
 def showCatalog():
     state = generateState(login_session, 'state')
     categories = session.query(Category).all()
-    return render_template('allCategories.html', categories=categories, STATE=state,
-                           session=login_session)
+    return render_template('allCategories.html', categories=categories,
+                           STATE=state, session=login_session)
 
 
 @app.route('/catalog/new', methods=['GET', 'POST'])
@@ -160,7 +190,8 @@ def newCategory():
             flash("Please log in!")
             return redirect(url_for('showCatalog'))
     else:
-        user = session.query(User).filter_by(email=login_session['email']).one()
+        user = session.query(User).filter_by(email=login_session['email'])
+        user = user.one()
         user_id = user.id
         # check if the post request has the file part
         if not request.files['image']:
@@ -173,7 +204,8 @@ def newCategory():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(app.config['UPLOAD_FOLDER'] + filename)
-            newCategory = Category(name=request.form['name'], image=filename, user_id=user_id)
+            newCategory = Category(name=request.form['name'], image=filename,
+                                   user_id=user_id)
             session.add(newCategory)
             session.commit()
             flash("New category added!")
@@ -190,20 +222,23 @@ def editCategory(category_id):
             # Check if they are authorised to EDIT the category
             category = session.query(Category).filter_by(id=category_id).one()
             category_owner = category.user_id
-            user = session.query(User).filter_by(email=login_session['email']).one()
+            user = session.query(User).filter_by(email=login_session['email'])
+            user = user.one()
             user_id = user.id
             if category_owner != user_id:
                 flash("You are not authorised to edit this category...")
                 return redirect(url_for('showCatalog'))
             # If they are, redirect to this
             category_name = category.name
-            return render_template('editCategory.html', category_id=category_id,
+            return render_template('editCategory.html',
+                                   category_id=category_id,
                                    category_name=category_name, STATE=state)
         except KeyError:
             flash("Please log in!")
             return redirect(url_for('showCatalog'))
     else:
-        editedCategory = session.query(Category).filter_by(id=category_id).one()
+        editedCategory = session.query(Category).filter_by(id=category_id)
+        editedCategory = editedCategory.one()
         if request.form['name']:
             editedCategory.name = request.form['name']
         session.add(editedCategory)
@@ -221,20 +256,23 @@ def deleteCategory(category_id):
             # Check if they are authorised to DELETE the category
             category = session.query(Category).filter_by(id=category_id).one()
             category_owner = category.user_id
-            user = session.query(User).filter_by(email=login_session['email']).one()
+            user = session.query(User).filter_by(email=login_session['email'])
+            user = user.one()
             user_id = user.id
             if category_owner != user_id:
                 flash("You are not authorised to delete this category...")
                 return redirect(url_for('showCatalog'))
             # If they are, redirect to this
             category_name = category.name
-            return render_template('deleteCategory.html', category_id=category_id,
+            return render_template('deleteCategory.html',
+                                   category_id=category_id,
                                    category_name=category_name, STATE=state)
         except KeyError:
             flash("Please log in!")
             return redirect(url_for('showCatalog'))
     else:
-        deletedCategory = session.query(Category).filter_by(id=category_id).one()
+        deletedCategory = session.query(Category).filter_by(id=category_id)
+        deletedCategory = deletedCategory.one()
         session.delete(deletedCategory)
         session.commit()
         flash("Category successfully deleted!")
@@ -253,8 +291,9 @@ def showItems(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
     user = session.query(User).filter_by(id=category.user_id).one()
     user_email = user.email
-    return render_template('showItems.html', items=items, category_id=category_id,
-                           category=category, STATE=state, session=login_session,
+    return render_template('showItems.html', items=items,
+                           category_id=category_id, category=category,
+                           STATE=state, session=login_session,
                            user_email=user_email)
 
 
@@ -264,26 +303,31 @@ def newItem(category_id):
     if request.method == 'GET':
         try:
             user = login_session['username']
-            owner = session.query(User).filter_by(email=login_session['email']).one()
+            owner = session.query(User).filter_by(email=login_session['email'])
+            owner = owner.one()
             user_id = owner.id
             category = session.query(Category).filter_by(id=category_id).one()
-            # Double check that user is authorised to make an item in this category
+            # Double check that user is authorised to make an item
+            # in this category
             if category.user_id != user_id:
                 flash("Please log in!")
                 return redirect(url_for('showItems', category_id=category_id))
-            return render_template('newItem.html', category_id=category_id, STATE=state)
+            return render_template('newItem.html', category_id=category_id,
+                                   STATE=state)
         except KeyError:
             flash("Please log in!")
             return redirect(url_for('showItems', category_id=category_id))
     else:
-        user = session.query(User).filter_by(email=login_session['email']).one()
+        user = session.query(User).filter_by(email=login_session['email'])
+        user = user.one()
         user_id = user.id
         category = session.query(Category).filter_by(id=category_id).one()
         # Double check that user is authorised to make an item in this category
         if category.user_id != user_id:
-            flash("You are not authorised to create an item in this category...")
+            flash("You are not authorised to create an item in this category.")
             return redirect(url_for('showItems', category_id=category_id))
-        newItem = Item(title=request.form['title'], description=request.form['description'],
+        newItem = Item(title=request.form['title'],
+                       description=request.form['description'],
                        category_id=category_id, user_id=user_id)
         session.add(newItem)
         session.commit()
@@ -291,7 +335,8 @@ def newItem(category_id):
         return redirect(url_for('showItems', category_id=category_id))
 
 
-@app.route('/catalog/<int:category_id>/items/<int:item_id>/edit', methods=['GET', 'POST'])
+@app.route('/catalog/<int:category_id>/items/<int:item_id>/edit',
+           methods=['GET', 'POST'])
 def editItem(category_id, item_id):
     state = generateState(login_session, 'state')
     editedItem = session.query(Item).filter_by(id=item_id).one()
@@ -303,13 +348,15 @@ def editItem(category_id, item_id):
             category = session.query(Category).filter_by(id=category_id).one()
             category_name = category.name
             return render_template('editItem.html', categories=categories,
-                                   category_name=category_name, category_id=category_id,
-                                   item_id=item_id, item_name=item_name, STATE=state)
+                                   category_name=category_name,
+                                   category_id=category_id, item_id=item_id,
+                                   item_name=item_name, STATE=state)
         except KeyError:
             flash("Please log in!")
             return redirect(url_for('showItems', category_id=category_id))
     else:
-        user = session.query(User).filter_by(email=login_session['email']).one()
+        user = session.query(User).filter_by(email=login_session['email'])
+        user = user.one()
         user_id = user.id
         # Double check that user is authorised to make an item in this category
         if editedItem.user_id != user_id:
@@ -326,7 +373,8 @@ def editItem(category_id, item_id):
         return redirect(url_for('showItems', category_id=category_id))
 
 
-@app.route('/catalog/<int:category_id>/items/<int:item_id>/delete', methods=['GET', 'POST'])
+@app.route('/catalog/<int:category_id>/items/<int:item_id>/delete',
+           methods=['GET', 'POST'])
 def deleteItem(category_id, item_id):
     state = generateState(login_session, 'state')
     deletedItem = session.query(Item).filter_by(id=item_id).one()
@@ -334,13 +382,15 @@ def deleteItem(category_id, item_id):
         try:
             user = login_session['username']
             item_name = deletedItem.title
-            return render_template('deleteItem.html', category_id=category_id, item_id=item_id,
-                                   item_name=item_name, STATE=state)
+            return render_template('deleteItem.html', category_id=category_id,
+                                   item_id=item_id, item_name=item_name,
+                                   STATE=state)
         except KeyError:
             flash("Please log in!")
             return redirect(url_for('showItems', category_id=category_id))
     else:
-        user = session.query(User).filter_by(email=login_session['email']).one()
+        user = session.query(User).filter_by(email=login_session['email'])
+        user = user.one()
         user_id = user.id
         # Double check that user is authorised to make an item in this category
         if deletedItem.user_id != user_id:
